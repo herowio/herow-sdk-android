1b5e7167f7a776f185d1b2d0e948f0500b3b373b
damien <dfe@connecthings.com>
2022-02-02 11:51:57 +0100

    Try to fixes ioexception in interceptor

--- detection/src/main/java/io/herow/sdk/detection/session/SessionInterceptor.kt
+++ detection/src/main/java/io/herow/sdk/detection/session/SessionInterceptor.kt
@@ -7,7 +7,9 @@ import io.herow.sdk.connection.SessionHolder
 import io.herow.sdk.detection.BuildConfig
 import io.herow.sdk.detection.koin.ICustomKoinComponent
 import okhttp3.Interceptor
+import okhttp3.Protocol
 import okhttp3.Response
+import okhttp3.ResponseBody.Companion.toResponseBody
 import org.koin.core.component.inject
 
 /**
@@ -22,29 +24,40 @@ class SessionInterceptor: Interceptor, ICustomKoinComponent {
     private val sessionHolder: SessionHolder by inject()
 
     override fun intercept(chain: Interceptor.Chain): Response {
-        val requestBuilder = chain.request().newBuilder()
-        requestBuilder.addHeader(HerowHeaders.SDK_VERSION_HEADER, BuildConfig.SDK_VERSION)
-        val sdkID = sessionHolder.getSDKID()
+        try {
+            val requestBuilder = chain.request().newBuilder()
+            requestBuilder.addHeader(HerowHeaders.SDK_VERSION_HEADER, BuildConfig.SDK_VERSION)
+            val sdkID = sessionHolder.getSDKID()
 
-        if (sdkID.isNotEmpty()) {
-            requestBuilder.addHeader(HerowHeaders.SDK_HEADER, sdkID)
+            if (sdkID.isNotEmpty()) {
+                requestBuilder.addHeader(HerowHeaders.SDK_HEADER, sdkID)
+            }
+
+            val accessToken = sessionHolder.getAccessToken()
+            if (accessToken.isNotEmpty()) {
+                requestBuilder.addHeader(HerowHeaders.AUTHORIZATION_HEADER, accessToken)
+            }
+
+            val herowId = sessionHolder.getHerowId()
+            if (herowId.isNotEmpty()) {
+                requestBuilder.addHeader(HerowHeaders.HEROW_ID_HEADER, herowId)
+            }
+
+            val deviceId = sessionHolder.getDeviceId()
+            if (deviceId.isNotEmpty()) {
+                requestBuilder.addHeader(HerowHeaders.DEVICE_ID_HEADER, deviceId)
+            }
+
+            return chain.proceed(requestBuilder.build())
+        } catch ( e: Throwable) {
+
+                return Response.Builder()
+                    .request(chain.request())
+                    .protocol(Protocol.HTTP_1_1)
+                    .code(400)
+                    .message("somethind went wrong")
+                    .body("client config invalid".toResponseBody(null))
+                    .build()
         }
-
-        val accessToken = sessionHolder.getAccessToken()
-        if (accessToken.isNotEmpty()) {
-            requestBuilder.addHeader(HerowHeaders.AUTHORIZATION_HEADER, accessToken)
-        }
-
-        val herowId = sessionHolder.getHerowId()
-        if (herowId.isNotEmpty()) {
-            requestBuilder.addHeader(HerowHeaders.HEROW_ID_HEADER, herowId)
-        }
-
-        val deviceId = sessionHolder.getDeviceId()
-        if (deviceId.isNotEmpty()) {
-            requestBuilder.addHeader(HerowHeaders.DEVICE_ID_HEADER, deviceId)
-        }
-
-        return chain.proceed(requestBuilder.build())
     }
 }
